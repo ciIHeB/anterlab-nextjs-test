0b01dcd816f3ab3468c7c17e051497466b4b25c5
ciIHeB <153993514+ciIHeB@users.noreply.github.com>
2025-06-27 14:44:51 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 export default function HomePage() {
   return (
-    <div>
+    <main className="max-w-3xl mx-auto p-6">
       <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
       <p>This is the homepage of your practical Next.js project.</p>
-    </div>
+    </main>
   );
 }
